2bbfdcc622bfbcb4d6b972c13a97a3746eaaf991
codeEgn <edcode010@gmail.com>
2024-03-13 11:30:23 +0300

    this is a ui update

--- interface.py
+++ interface.py
@@ -9,14 +9,6 @@ css = '''
 .chat-message.bot {
     background-color: #475063
 }
-.chat-message .avatar {
-  width: 20%;
-}
-.chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
 }
 .chat-message .message {
   width: 80%;
@@ -27,18 +19,12 @@ css = '''
 
 bot_template = '''
 <div class="chat-message bot">
-    <div class="avatar">
-        <img src="./avatars/bot.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
-    </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
 user_template = '''
-<div class="chat-message user">
-    <div class="avatar">
-        <img src="./avatars/human.jpg">
-    </div>    
+<div class="chat-message user"> 
     <div class="message">{{MSG}}</div>
 </div>
 '''
